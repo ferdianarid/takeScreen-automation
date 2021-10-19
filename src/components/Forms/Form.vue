b1976495ee6532ab21510f5f6df67812e8a1a49a
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-10-19 13:58:24 +0700

    getdata

--- src/components/Forms/Form.vue
+++ src/components/Forms/Form.vue
@@ -37,10 +37,11 @@
 			</div>
 		</div>
 		<!-- Component End  -->
-		<button @click="postData()" class="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 ml-3 mt-5 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 hover:bg-emerald-800" type="button">
-			<i class="fas fa-paper-plane"></i>&nbsp;
-			Send Data
-		</button>
+		<VueLoadingButton
+			aria-label="Post Data"
+			class="button bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 ml-3 mt-5 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 hover:bg-emerald-800"
+			:loading="isLoading" @click="handleClick()" type="button">
+			<i class="fas fa-paper-plane"></i>Send Data</VueLoadingButton>
 		</div>
 	</div>
 </div>
@@ -48,6 +49,7 @@
 
 <script>
 import axios from "axios";
+import VueLoadingButton from "vue-loading-button";
 
 export default {
 	name: "Form",
@@ -55,10 +57,16 @@ export default {
 		return {
 			domain: "",
 			username: "",
-			password: ""
+			password: "",
+			isLoading: false,
 	};
 },
 methods: {
+	handleClick() {
+		console.log("click");
+		this.isLoading = true;
+		setTimeout(() => (this.isLoading = false), 3000);
+    },
 async postData() {
 	try {
 		await axios.post("119.8.175.1:5050/data", {
@@ -102,7 +110,10 @@ onClick() {
    
                      fileLink.click();
                 });
-          }
+          },
+	   components: {
+		VueLoadingButton
+	}
 };
 </script>
 <style></style>
