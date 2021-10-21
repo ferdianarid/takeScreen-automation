dae69f202ccf75bf5628ce08d37d77add0b763ed
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2021-10-21 08:23:28 +0700

    remove function

--- src/components/Forms/Form.vue
+++ src/components/Forms/Form.vue
@@ -37,11 +37,10 @@
 			</div>
 		</div>
 		<!-- Component End  -->
-		<VueLoadingButton
-			aria-label="Post Data"
-			class="button bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 ml-3 mt-5 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 hover:bg-emerald-800"
-			:loading="isLoading" @click="handleClick()" type="button">
-			<i class="fas fa-paper-plane"></i>Send Data</VueLoadingButton>
+		<button @click="postData()" class="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 ml-3 mt-5 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 hover:bg-emerald-800" type="button">
+			<i class="fas fa-paper-plane"></i>&nbsp;
+			Send Data
+		</button>
 		</div>
 	</div>
 </div>
@@ -49,7 +48,6 @@
 
 <script>
 import axios from "axios";
-import VueLoadingButton from "vue-loading-button";
 
 export default {
 	name: "Form",
@@ -57,63 +55,28 @@ export default {
 		return {
 			domain: "",
 			username: "",
-			password: "",
-			isLoading: false,
-	};
-},
-methods: {
-	handleClick() {
-		console.log("click");
-		this.isLoading = true;
-		setTimeout(() => (this.isLoading = false), 3000);
-    },
-async postData() {
-	try {
-		await axios.post("119.8.175.1:5050/data", {
-			domain: this.domain,
-			username: this.username,
-			password: this.password,
-		});
-			this.domain = "";
-			this.username = "";
-			this.password = "";
-			console.log("Success")
-		} catch (err) {
-			console.log(err);
-		} finally {
-			console.log('success')
+			password: ""
 		}
 	},
-},
-async getData() {
-	try {
-		await axios.get("119.8.175.1:5050/data")
-			console.log(data)
-		} catch (err) {
-			console.log(err);
-		} finally {
-			console.log('success')
+	methods: {
+		async postData() {
+			try {
+				await axios.post("119.8.175.1:5050/data", {
+					domain: this.domain,
+					username: this.username,
+					password: this.password,
+			});
+				this.domain = "";
+				this.username = "";
+				this.password = "";
+				console.log("Success")
+			} catch (err) {
+				console.log(err);
+			} finally {
+				console.log('success')
+			}
 		}
-	},
-onClick() {
-              axios({
-                    url: 'http://localhost:5000/data',
-                    method: 'GET',
-                    responseType: 'blob',
-                }).then((response) => {
-                     var fileURL = window.URL.createObjectURL(new Blob([response.data]));
-                     var fileLink = document.createElement('a');
-   
-                     fileLink.href = fileURL;
-                     fileLink.setAttribute('download', 'file.pdf');
-                     document.body.appendChild(fileLink);
-   
-                     fileLink.click();
-                });
-          },
-	   components: {
-		VueLoadingButton
 	}
-};
+}
 </script>
 <style></style>
